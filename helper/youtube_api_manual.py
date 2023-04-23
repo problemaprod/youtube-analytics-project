e40d6a43efcc54240c6d4715da4bdbc5f9002136
Mikhail Baranov <problemaprod@gmail.com>
2023-04-23 15:35:58 +0100

    написал методы для класса Channel

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -1,11 +1,10 @@
 import json
 import os
-
+import requests
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
 import isodate
-
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
@@ -25,8 +24,8 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
-channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
+channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
+# channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 printj(channel)
 
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,31 @@
+from googleapiclient.discovery import build
+import os
+import json
+
+api_key: str = os.getenv('YT_API_KEY')
+print(api_key)
+
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        # return channel
+
+
+# two_drots = Channel("UCOIRN19VunfPaW7ZfmOKeoQ")
+#
+#
+# # two_drots.print_info()
+
+
+
