309fa81bfdf61e37bbda7002944b45031816ccbd
Mikhail Baranov <problemaprod@gmail.com>
2023-05-01 17:30:35 +0100

    Дописал классы и методы для выполнения 2 домашнего задания

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,6 @@ import os
 import json
 
 api_key: str = os.getenv('YT_API_KEY')
-print(api_key)
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -13,19 +12,37 @@ class Channel:
 
     def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.content = json.dumps(self.channel, indent=2, ensure_ascii=False)
+        self.file = json.loads(self.content)
+        self.title = dict(self.file)['items'][0]['snippet']['title']
+        self.video_count = dict(self.file)['items'][0]['statistics']['videoCount']
+        self.url = f"https://www.youtube.com/channel/{dict(self.file)['items'][0]['id']}"
 
     def print_info(self):
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
-        # return channel
+        self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        return json.dumps(self.channel, indent=2, ensure_ascii=False)
 
+    @property
+    def channel_id(self):
+        """Делаеи channel_id не изменяемым"""
+        return self.__channel_id
+
+    @staticmethod
+    def get_service():
+        """получаем объект для работы с API вне класса"""
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, file_name: str):
+        """сохроняем данные каналла в в файле"""
+        with open(file_name, 'w') as f:
+            json.dump(self.channel, f, indent=2, ensure_ascii=False)
 
 # two_drots = Channel("UCOIRN19VunfPaW7ZfmOKeoQ")
-#
-#
-# # two_drots.print_info()
-
-
-
+# # print(two_drots.print_info())
+# # print(two_drots.url)
+# # two_drots.channel_id = 'Новое название'
+# # print(two_drots.channel_id)
+# two_drots.to_json('two_drots.json')
