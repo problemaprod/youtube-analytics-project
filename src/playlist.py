4984b90c3dcfbb53fc1c158cad5ca728c8511455
Mikhail Baranov <problemaprod@gmail.com>
2023-05-14 13:37:49 +0100

    Выполнил 5 домашнее задание

---
+++ src/playlist.py
@@ -0,0 +1,70 @@
+import json
+import os
+import requests
+from datetime import datetime, time, timedelta
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+
+import isodate
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+class PlayList:
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.playlist_stat = youtube.playlists().list(part='snippet', id=self.playlist_id, maxResults=50).execute()
+        self.title = self.playlist_stat['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+
+    @property
+    def total_duration(self):
+        __total_duration = timedelta(0)
+        playlist_videos = youtube.playlistItems().list(playlistId=self.playlist_id,
+                                                       part='contentDetails',
+                                                       maxResults=50,
+                                                       ).execute()
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                               id=','.join(video_ids)
+                                               ).execute()
+        for video in video_response['items']:
+            # YouTube video duration is in ISO 8601 format
+            iso_8601_duration = video['contentDetails']['duration']
+            duration = isodate.parse_duration(iso_8601_duration)
+            __total_duration += duration
+        return __total_duration
+
+    def show_best_video(self):
+        playlist_videos = youtube.playlistItems().list(playlistId=self.playlist_id,
+                                                       part='contentDetails',
+                                                       maxResults=50,
+                                                       ).execute()
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                               id=','.join(video_ids)
+                                               ).execute()
+        liked_video = 0
+
+        for video in video_response['items']:
+            like_count: int = int(video['statistics']['likeCount'])
+            if like_count > liked_video:
+                liked_video = like_count
+            else:
+                continue
+
+        for video in video_response['items']:
+            if str(liked_video) in video['statistics'].values():
+                # return video
+                video_id = video['id']
+
+        show_best_video_url = f"https://youtu.be/{video_id}"
+        return show_best_video_url
+
+
+pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+print(pl.show_best_video)
